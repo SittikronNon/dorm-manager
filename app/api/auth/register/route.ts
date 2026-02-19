fc3693d6add99620785fd48758cd99c2042b7926
SittikronNon <sittikron.lamulsab@gmail.com>
2026-02-19 21:51:32 +0700

    update registration to be more secure for in-house usage

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -4,17 +4,23 @@ import bcrypt from "bcrypt"
 
 
 export async function POST(request: Request) {
-const { username, password } = await request.json();
-    if (!username || !password) return NextResponse.json({ message: 'login failed, please fill both username and password!' }, {status:409})
+    const { username, password, registerSecret } = await request.json();
+    if (!username || !password) return NextResponse.json({ message: 'Registration failed, please provide both username and password!' }, { status: 400 })
+    if (registerSecret !== process.env.REGISTER_SECRET) {
+        return NextResponse.json(
+            { error: "Unauthorized: Invalid Secret Key" },
+            { status: 401 }
+        );
+    }
     try {
         const saltRounds = 12;
         const hashPassword = await bcrypt.hash(password, saltRounds);
         const query = `INSERT INTO users (username, password) VALUES ($1, $2)`
         await pool.query(query, [username, hashPassword])
-        return NextResponse.json({message: 'Registered'})
+        return NextResponse.json({ message: 'Registered' })
 
     } catch (err) {
         console.error(err)
-        return NextResponse.json({ message: 'user not found!' }, {status:409})
+        return NextResponse.json({ message: 'user not found!' }, { status: 409 })
     }
 }
